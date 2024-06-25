7c856108d489a96cccefe239bdbd3415ac665edb
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2024-06-25 05:50:56 -0500

    Fix: modified connection string

--- obj/Debug/net8.0/EnergyXChain.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnergyXChain.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef92cd8f55f33595ba3e6a9ddf3c09c12fb40fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
