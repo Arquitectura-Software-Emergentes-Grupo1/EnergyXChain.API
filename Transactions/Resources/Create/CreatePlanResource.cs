1d9511c96d0d840e023dcaa176c27e3146b7a68b
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2024-06-25 02:44:12 -0500

    feat

--- Transactions/Resources/Create/CreatePlanResource.cs
+++ Transactions/Resources/Create/CreatePlanResource.cs
@@ -4,6 +4,6 @@ public class CreatePlanResource
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public long Fee { get; set; }
+    public double Fee { get; set; }
     public int SupplierId { get; set; }
 }
--- obj/Debug/net8.0/EnergyXChain.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnergyXChain.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addc4dc711c9077d97812d072bedd52c47cd91ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46900e12b007a81d50eab74872168be03e8ab84e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyXChain.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
