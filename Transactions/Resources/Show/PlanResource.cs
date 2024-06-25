46900e12b007a81d50eab74872168be03e8ab84e
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2024-06-25 02:02:56 -0500

    feat

--- Transactions/Resources/Show/PlanResource.cs
+++ Transactions/Resources/Show/PlanResource.cs
@@ -5,6 +5,6 @@ public class PlanResource
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public long Fee { get; set; }
+    public double Fee { get; set; }
     public SupplierResource Supplier { get; set; }
 }
